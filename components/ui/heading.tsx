d0cae7a68fb45ba369db2e9cb329709ce06035c6
lnirjar <lnirjar@gmail.com>
2023-10-10 04:41:14 +0530

    completed settings page

---
+++ components/ui/heading.tsx
@@ -0,0 +1,22 @@
+interface HeadingProps {
+  title: string;
+  description: string;
+}
+
+import React from 'react'
+
+export const Heading: React.FC<HeadingProps> = ({
+  title,
+  description
+}) => {
+  return (
+    <div>
+      <h2 className='text-3xl font-bold tracking-tight'>
+        {title}
+      </h2>
+      <p className='text-sm text-muted-foreground'>
+        {description}
+      </p>
+    </div>
+  )
+}
