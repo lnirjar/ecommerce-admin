d415ef4f0f8150082e44f97ab5068644b395f237
lnirjar <lnirjar@gmail.com>
2023-10-27 16:59:36 +0530

    completed categories

--- app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -7,14 +7,14 @@ import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
-import { BillboardColumn, columns } from "./columns";
+import { CategoryColumn, columns } from "./columns";
 import { ApiList } from "@/components/ui/api-list";
 
-interface BillboardClientProps {
-  data: BillboardColumn[]
+interface CategoryClientProps {
+  data: CategoryColumn[]
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({
+export const CategoryClient: React.FC<CategoryClientProps> = ({
   data
 }) => {
   const router = useRouter();
@@ -24,27 +24,27 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title={`Billboards (${data.length})`}
-          description="Manage billboards for your store"
+          title={`Categories (${data.length})`}
+          description="Manage categories for your store"
         />
         <Button
-          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}
         >
           <PlusIcon className="mr-2 h-4 w-4" />
           Add New
         </Button>
       </div>
       <Separator />
-      <DataTable searchKey="label" columns={columns} data={data} />
+      <DataTable searchKey="name" columns={columns} data={data} />
 
       <Heading
         title="API"
-        description="API calls for billboards"
+        description="API calls for categories"
       />
       <Separator />
       <ApiList
-        entityName="billboards"
-        entityIdName="billboardId"
+        entityName="categories"
+        entityIdName="categoryId"
       />
     </>
   )
